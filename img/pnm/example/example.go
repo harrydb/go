c1f599c1b57cd101b6ab802782f71b388f7538af
Kevin Gillette <extemporalgenome@gmail.com>
2013-10-31 14:34:20 -0600

    example.go now builds, and is more idiomatic

--- img/pnm/example/example.go
+++ img/pnm/example/example.go
@@ -11,28 +11,33 @@ import (
 	"os"
 )
 
-func Main() {
-	file, err := os.Open("in.ppm")
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalln("usage:", os.Args[0], "input-file output-file")
+	}
+	in, out := os.Args[1], os.Args[2]
+	f, err := os.Open(in)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	img, format, err := image.Decode(file)
+	img, format, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Close()
 
-	log.Println("Format: ", format)
+	log.Println("Format:", format)
 
+	f, err = os.Create(out)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	outFile, err := os.Create("out.ppm")
+	err = pnm.Encode(f, img, pnm.PPM)
 	if err != nil {
+		os.Remove(out)
 		log.Fatal(err)
 	}
-
-	err = pnm.Encode(outFile, img, pnm.PPM)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	f.Close() // in some situations, this could be important for flushing remaining output.
 }
